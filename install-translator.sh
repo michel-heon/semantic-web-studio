e7bb65e0094e7e0cf660779d618a2bef78d1be20
michel-heon <heon.michel@uqam.ca>
2022-07-25 13:38:06 -0400

    maj script installation

--- install-translator.sh
+++ install-translator.sh
@@ -1,5 +1,4 @@
-#!/bin/bash 
-
+#!/bin/bash
 ###################################################################
 # Script Name   :
 # Description   : Stript d'installation du translator et de Jena
@@ -12,5 +11,8 @@
 ###################################################################
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P)"
 source $SCRIPT_DIR/00-env.sh
+cd $SCRIPT_DIR
+echo INSTALL to $TRANSLATOR_HOME with jena $JENA_VERSION
 mvn clean install -Dtranslator.deploy.dir=$TRANSLATOR_HOME
+cp 00-env.sh $TRANSLATOR_HOME
 chmod +x $TRANSLATOR_HOME/bin/*
