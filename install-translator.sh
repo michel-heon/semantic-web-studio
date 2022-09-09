638b1acd2efdbc56816675745349c2c397ab552a
michel-heon <heon.michel@uqam.ca>
2022-09-09 08:46:31 +0000

    création de show-vars.sh

--- install-translator.sh
+++ install-translator.sh
@@ -12,9 +12,11 @@
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P)"
 unset TRANSLATOR_HOME
 source $SCRIPT_DIR/00-env.sh
+$SCRIPT_DIR/show-vars.sh
 cd $SCRIPT_DIR
 echo INSTALL to $TRANSLATOR_HOME with jena $JENA_VERSION
 mvn clean install -Dtranslator.deploy.dir=$TRANSLATOR_HOME -Djena.version=$JENA_VERSION
 cp 00-env.sh $TRANSLATOR_HOME
 cp -r ldap-tools $TRANSLATOR_HOME
 chmod +x $TRANSLATOR_HOME/bin/*
+
